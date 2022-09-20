810a57312e25e4531744d3713892bde06912a733
rrvnccf <ichafebrin184@gmail.com>
2022-09-20 19:42:57 +0700

    ichaa

---
+++ src/views/Animals.vue
@@ -0,0 +1,17 @@
+<template>
+		<h1>this is an animal</h1>
+		<h1>{{animals}}</h1>
+</template>
+
+<script>
+	export default{
+		data(){
+			return {
+				animals : {gajah : "mamalia"}
+			}
+		},
+		created(){
+			console.log("haii")
+		}
+	}
+</script>
